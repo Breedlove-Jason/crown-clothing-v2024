935d011ea96261648b6ce88c45a152920b9f2fc0
Jason Breedlove <j@jasonbreedlove.com>
2024-09-18 21:16:52 -0700

    work on routing
    work on routing
    
    Added and updated dependencies for routing using React Router and Remix Run. Refactored App.jsx to use Routes and Route components for routing. Updated main.jsx to include BrowserRouter for routing. Created a new Home component in the routes directory with Outlet from react-router-dom. Added font styles for Open Sans Condensed in fonts.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
-import Directory from '@components/Directory/Directory';
+import Home from './routes/Home/Home.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'women',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'men',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
   return (
     <>
-      <Directory categories={categories} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }

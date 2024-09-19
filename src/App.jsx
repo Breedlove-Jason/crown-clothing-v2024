97b8d77f1ce18fd35d99672b3f4fcf370ddba7a1
Breedlove-Jason <j@jasonbreedlove.com>
2024-09-19 13:50:56 -0700

    create navbar component with minor styles
    proceed to firebase setup, config, and implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import Home from './routes/Home/Home.jsx';
-import { Routes, Route } from 'react-router-dom';
+import Home from "./routes/Home/Home.jsx";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/Navigation/Navigation.jsx";
+import Shop from "./routes/Shop/Shop.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path={"shop"} element={<Shop />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import CrownLogo from "../../assets/crown.svg";
+import "./Navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className={"navigation"}>
+        <Link className={"logo-container"} to={"/"}>
+          <img src={CrownLogo} className={"logo"} alt={"Crown Clothing Logo"} />
+        </Link>
+        <div className={"nav-links-container"}>
+          <Link className={"nav-link"} to="/shop">
+            SHOP
+          </Link>
+          {/*<Link className={"nav-link"} to=""></Link>*/}
+          {/*<Link className={"nav-link"} to=""></Link>*/}
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
